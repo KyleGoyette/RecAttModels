bc7d4d678c4bcea4ae13a878d7cd3351fee89bff
KyleGoyette <kdgoyette@gmail.com>
2020-05-28 12:53:07 -0700

    experiment now says config instead of args

--- experiment.py
+++ experiment.py
@@ -18,10 +18,10 @@ import torch.nn as nn
 
 class Experiment(object):
 
-    def __init__(self, args):
-        # for each item in args that is a list, select current item.
+    def __init__(self, config):
+        # for each item in config that is a list, select current item.
 
-        save_dir = args.save_dir
+        save_dir = config.save_dir
         if not os.path.exists(save_dir):
             create_exp_dir(save_dir)
 
@@ -31,15 +31,15 @@ class Experiment(object):
         state_file_list = natsorted([fn for fn in os.listdir(save_dir)
                                      if fn.endswith('.pt')])
 
-        model = self._get_model(args)
-        if args.model not in ['ORNN']:
-            optimizer = self._get_optimizer(args, model.parameters())
+        model = self._get_model(config)
+        if config.model not in ['ORNN']:
+            optimizer = self._get_optimizer(config, model.parameters())
         else:
             non_orth_parameters, log_orth_parameters = get_parameters(model)
-            normal_opt = self._get_optimizer(args, non_orth_parameters)
-            orth_opt = self._get_optimizer(args,log_orth_parameters,orth=True)
+            normal_opt = self._get_optimizer(config, non_orth_parameters)
+            orth_opt = self._get_optimizer(config,log_orth_parameters,orth=True)
             optimizer = (normal_opt, orth_opt)
-        scheduler = self._get_scheduler(args, optimizer)
+        scheduler = self._get_scheduler(config, optimizer)
 
         # if path exists and some model has been saved in it, Load experiment
         if os.path.exists(save_dir) and len(state_file_list):
@@ -60,13 +60,13 @@ class Experiment(object):
             (self.train_losses, self.train_accs, self.val_losses,
              self.val_accs, self.val_hist) = ([] for i in range(5))
 
-        self.args = args
+        self.args = config
         self.model = model
-        if args.device is not None:
-            self.model = self.model.to(args.device)
+        if config.device is not None:
+            self.model = self.model.to(config.device)
         self.optimizer = optimizer
         self.scheduler = scheduler
-        if args.cuda:
+        if config.cuda:
             self.model = self.model.cuda()
 
             if self.optimizer is not None:
@@ -76,25 +76,25 @@ class Experiment(object):
                             state[k] = v.cuda()
 
     # Adapted from https://github.com/veugene/DemiGORU
-    def _get_optimizer(self, args, params, orth=False):
-        name = args.opt
+    def _get_optimizer(self, config, params, orth=False):
+        name = config.opt
         kwargs = {'params'       : [p for p in params if p.requires_grad]}
         if not orth:
-            if args.lr is not None:
-                kwargs.update({'lr': args.lr})
+            if config.lr is not None:
+                kwargs.update({'lr': config.lr})
         else:
-            if args.lr_orth is not None:
-                kwargs.update({'lr': args.lr_orth})
+            if config.lr_orth is not None:
+                kwargs.update({'lr': config.lr_orth})
             # automatically set to 1/10th of normal optimizer
-            elif args.lr is not None:
-                kwargs.update({'lr': args.lr/10})
+            elif config.lr is not None:
+                kwargs.update({'lr': config.lr/10})
         if name.lower() == 'rmsprop':
-            if args.betas is not None:
-                kwargs.update({'alpha': args.alpha})
+            if config.betas is not None:
+                kwargs.update({'alpha': config.alpha})
             optimizer = torch.optim.RMSprop(**kwargs)
         elif name.lower() == 'adam':
-            if args.alpha is not None:
-                kwargs.update({'betas': args.betas})
+            if config.alpha is not None:
+                kwargs.update({'betas': config.betas})
             optimizer = torch.optim.Adam(**kwargs)
         elif name.lower() == 'sgd':
             # add lr if none, required for sgd
@@ -105,127 +105,127 @@ class Experiment(object):
             raise ValueError("Optimizer {} not supported.".format(name))
         return optimizer
 
-    def _get_model(self, args):
-        model_name = args.model
+    def _get_model(self, config):
+        model_name = config.model
         if model_name == 'RNN':
-            base = nn.RNNCell(input_size=args.input_size,
-                              hidden_size=args.nhid,
-                              nonlinearity=args.nonlin)
+            base = nn.RNNCell(input_size=config.input_size,
+                              hidden_size=config.nhid,
+                              nonlinearity=config.nonlin)
         elif model_name == 'ORNN':
-            base = OrthogonalRNN(input_size=args.input_size,
-                                 hidden_size=args.nhid,
+            base = OrthogonalRNN(input_size=config.input_size,
+                                 hidden_size=config.nhid,
                                  initializer_skew=henaff_init_,
                                  mode='static',
                                  param=expm)
         elif model_name == 'LSTM':
-            base = nn.LSTMCell(input_size=args.input_size,
-                               hidden_size=args.nhid)
+            base = nn.LSTMCell(input_size=config.input_size,
+                               hidden_size=config.nhid)
         elif model_name == 'MemRNN':
-            base = MemRNN(input_size=args.input_size,
-                          hidden_size=args.nhid,
-                          nonlinearity=args.nonlin,
-                          device=args.device)
+            base = MemRNN(input_size=config.input_size,
+                          hidden_size=config.nhid,
+                          nonlinearity=config.nonlin,
+                          device=config.device)
         elif model_name == 'SAB':
-            model = SAB_LSTM(input_size=args.input_size,
-                             hidden_size=args.nhid,
-                             num_layers=args.nlayers,
-                             num_classes=args.n_labels+1,
-                             truncate_length=args.trunc,
-                             attn_every_k=args.attk,
-                             top_k=args.topk,
-                             device=args.device)
+            model = SAB_LSTM(input_size=config.input_size,
+                             hidden_size=config.nhid,
+                             num_layers=config.nlayers,
+                             num_classes=config.n_labels+1,
+                             truncate_length=config.trunc,
+                             attn_every_k=config.attk,
+                             top_k=config.topk,
+                             device=config.device)
             return model
         elif model_name == 'Trans':
-            base = Transformer(d_model=args.input_size,
-                                nhead=args.nhead,
-                                num_encoder_layers=args.nenc,
-                                num_decoder_layers=args.ndec,
-                                dim_feedforward=args.nhid,
+            base = Transformer(d_model=config.input_size,
+                                nhead=config.nhead,
+                                num_encoder_layers=config.nenc,
+                                num_decoder_layers=config.ndec,
+                                dim_feedforward=config.nhid,
                                 dropout=0)
         else:
             raise ValueError('Model {} not supported.'.format(model_name))
 
-        if args.task == 'copy' or args.task == 'denoise':
+        if config.task == 'copy' or config.task == 'denoise':
             if model_name == 'Trans':
                 model = base
             else:
                 model = RecurrentCopyModel(base,
-                                           args.nhid,
-                                           args.onehot,
-                                           args.n_labels,
-                                           device=args.device)
+                                           config.nhid,
+                                           config.onehot,
+                                           config.n_labels,
+                                           device=config.device)
         else:
-            raise ValueError('Task {} not supported.'.format(args.task))
+            raise ValueError('Task {} not supported.'.format(config.task))
         return model
 
-    def _get_scheduler(self, args, optimizer):
-        if vars(args).get('sch_kwargs', None) is None:
+    def _get_scheduler(self, config, optimizer):
+        if vars(config).get('sch_kwargs', None) is None:
             return None
-        kwargs = args.sch_kwargs
+        kwargs = config.sch_kwargs
         scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer,
                                                                **kwargs)
         return scheduler
 
 
 class NMTExperiment(Experiment):
-    def _get_model(self, args):
-        model_name = args.model
+    def _get_model(self, config):
+        model_name = config.model
         if model_name == 'RNN':
-            encoder = RNNEncoder(inp_size=args.inp_size,
-                                 emb_size=args.demb,
-                                 hid_size=args.nhid,
-                                 n_layers=args.nenc,
-                                 dropout=args.dropout)
+            encoder = RNNEncoder(inp_size=config.inp_size,
+                                 emb_size=config.demb,
+                                 hid_size=config.nhid,
+                                 n_layers=config.nenc,
+                                 dropout=config.dropout)
 
-            decoder = RNNDecoder(out_size=args.out_size,
-                                 emb_size=args.demb,
-                                 hid_size=args.nhid,
-                                 n_layers=args.ndec,
-                                 dropout=args.dropout)
+            decoder = RNNDecoder(out_size=config.out_size,
+                                 emb_size=config.demb,
+                                 hid_size=config.nhid,
+                                 n_layers=config.ndec,
+                                 dropout=config.dropout)
 
             model = Seq2Seq(encoder=encoder, decoder=decoder)
 
         elif model_name == 'MemRNN':
-            encoder = BidirectionalEncoder(inp_size=args.inp_size,
-                                           emb_size=args.demb,
-                                           enc_hid_size=args.nhid,
-                                           dec_hid_size=args.nhid,
-                                           dropout=args.dropout)
+            encoder = BidirectionalEncoder(inp_size=config.inp_size,
+                                           emb_size=config.demb,
+                                           enc_hid_size=config.nhid,
+                                           dec_hid_size=config.nhid,
+                                           dropout=config.dropout)
 
 
-            attention = Attention(enc_hid_size=args.nhid,
-                                  dec_hid_size=args.nhid)
+            attention = Attention(enc_hid_size=config.nhid,
+                                  dec_hid_size=config.nhid)
 
-            decoder = BidirectionalDecoder(out_size=args.out_size,
-                                           emb_size=args.demb,
-                                           enc_hid_size=args.nhid,
-                                           dec_hid_size=args.nhid,
-                                           dropout=args.dropout,
+            decoder = BidirectionalDecoder(out_size=config.out_size,
+                                           emb_size=config.demb,
+                                           enc_hid_size=config.nhid,
+                                           dec_hid_size=config.nhid,
+                                           dropout=config.dropout,
                                            attention=attention)
 
             model = AttnSeq2Seq(encoder=encoder,
                                 decoder=decoder)
 
         elif model_name == 'Trans':
-            encoder = TransformerEncoder(inp_size=args.inp_size,
-                                         hid_size=args.nhid,
-                                         n_layers=args.nenc,
-                                         n_heads=args.nhenc,
+            encoder = TransformerEncoder(inp_size=config.inp_size,
+                                         hid_size=config.nhid,
+                                         n_layers=config.nenc,
+                                         n_heads=config.nhenc,
                                          pf_dim=512,
-                                         dropout=args.dropout,
+                                         dropout=config.dropout,
                                          max_length=100)
 
-            decoder = TransformerDecoder(output_dim=args.out_size,
-                                         hid_size=args.nhid,
-                                         n_layers=args.ndec,
-                                         n_heads=args.nhdec,
+            decoder = TransformerDecoder(output_dim=config.out_size,
+                                         hid_size=config.nhid,
+                                         n_layers=config.ndec,
+                                         n_heads=config.nhdec,
                                          pf_dim=512,
-                                         dropout=args.dropout,
+                                         dropout=config.dropout,
                                          max_length=100)
             model = TransformerSeq2Seq(encoder=encoder,
                                        decoder=decoder,
-                                       src_pad_idx=args.SRCPADIDX,
-                                       trg_pad_idx=args.TRGPADIDX)
+                                       src_pad_idx=config.SRCPADIDX,
+                                       trg_pad_idx=config.TRGPADIDX)
 
         else:
             raise ValueError('Model {} not supported.'.format(model_name))
