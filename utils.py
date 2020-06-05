f06c0ecbc35041ae8fa8c542da675cf6d8ce9d87
KyleGoyette <kdgoyette@gmail.com>
2020-06-05 13:50:00 -0700

    WIP start of grad plot

--- utils.py
+++ utils.py
@@ -1,7 +1,11 @@
 import torch
 import numpy as np
-import random
-import time
+import altair as alt
+import altair_viewer
+import json
+from scipy.special import softmax
+import pandas as pd
+
 def generate_copy_batch(delay, n_labels, seq_length, batch_size):
     seq = torch.randint(low=1,
                         high=n_labels+1,
@@ -52,3 +56,79 @@ def normal(tensor, mean=0, std=1):
         return tensor
     else:
         return tensor.normal_(mean, std)
+
+
+def grad_visualization(grads, attention):
+    """ Produces a vega-lite plot for visualizing gradients.
+    :param grads: list of length T gradients values
+    :param attention: T x T matrix of attention weights (lower triangular
+    :return:
+    """
+
+    select_point = alt.selection_single(
+        on='mouseover', nearest=True, fields=['x1', 'y1'], empty="none"
+    )
+
+    attention_jsonlist = attention_matrix_to_jsonlist(attention, grads)
+    attention_data = alt.Data(values=attention_jsonlist)
+    #print(attention_data)
+    attention_data_jsoned = {}
+    for i in range(len(attention_jsonlist)):
+        attention_data_jsoned.update({'id': i,
+                                      'x2': attention_jsonlist[i]['x2']
+                                      })
+
+    lookup_data = alt.LookupData(attention_data, key='id')
+    chart2 = alt.Chart(attention_data).mark_rule().encode(
+        x='x1:T',
+        y='y1:Q',
+        x2='x2:Q',
+        y2='y2:Q',
+        opacity='bloop:Q'
+    ).transform_filter(
+        select_point
+    )
+
+    grads_json = json.dumps([{'x': i, 'y': j} for i, j in enumerate(grads)])
+    grads_data = alt.Data(values=grads_json)
+    chart1 = alt.Chart(grads_data).mark_circle().encode(
+        x='x:T',
+        y='y:Q',
+    ).transform_lookup(
+        lookup="x2",
+        from_=lookup_data
+    ).add_selection(
+        select_point
+    )
+
+    (chart2 + chart1).resolve_scale(x='shared', y='shared').show()
+
+def attention_matrix_to_jsonlist(attention, grads):
+    print(attention.shape, len(grads))
+    rows = []
+    for i in range(len(grads)):
+        rows += create_row(attention[i, :i], grads, i)
+    return rows
+
+
+def create_row(attn_row, grads, t):
+    row = []
+    for i, val in enumerate(list(attn_row)):
+        linedef = {
+            'x1': t,
+            'y1': grads[t],
+            'x2': i,
+            'y2': grads[i],
+            'bloop': attn_row[i]
+        }
+        row.append(linedef)
+    return row
+
+T=10
+np.random.seed(1)
+x = [np.random.random(1)[0] for i in range(T)]
+y = softmax(np.tril(np.random.random((T, T)), 0), axis=1)
+y = np.where(y > 0.15, y, 0)
+y[1,0] = 1
+print(y)
+grad_visualization(x,y)
